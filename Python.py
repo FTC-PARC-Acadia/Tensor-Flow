b973b2ca6cf7e9b08be8a4c8dd312aba2d5c7d05
Chemi <abbyandam@gmail.com>
2022-10-10 08:18:56 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,8 +1,22 @@
+from distutils.log import info
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+from keras import layers
+from keras.models import Sequential
+
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
+PIL.Image.open("Green.png")
+
+
