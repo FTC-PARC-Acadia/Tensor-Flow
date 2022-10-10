9f8456ad5494f27a471a02a0ce7bc9a59e9f3be1
Chemi <abbyandam@gmail.com>
2022-10-10 16:10:39 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,8 @@
+from cProfile import label
 from distutils.log import info
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
-import PIL
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
@@ -10,13 +11,15 @@ from keras import layers
 from keras.models import Sequential
 
 import pathlib
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
+# dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+# data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+# data_dir = pathlib.Path(data_dir)
 
-roses = list(data_dir.glob('roses/*'))
-PIL.Image.open(str(roses[0]))
+ds = tfds.load('emnist/letters', split='train', shuffle_files=True)
 
-PIL.Image.open("Green.png")
+# roses = list(data_dir.glob('roses/*'))
+# img = mpimg.imread(str(roses[0]))
+# imgplot = plt.imshow(img)
+# plt.show()
 
 
