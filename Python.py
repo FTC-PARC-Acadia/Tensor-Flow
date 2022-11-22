52974ed14fbbbf3965dc60c19a07d22eca329fe0
Chemi <abbyandam@gmail.com>
2022-11-22 13:35:29 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -25,43 +25,43 @@ x_test = x_test.astype('float32') / 255
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
 
-# model = keras.Sequential(
-#     [
-#         keras.Input(shape=(28, 28, 1)),
-#         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-#         layers.MaxPooling2D(pool_size=(2, 2)),
-#         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-#         layers.MaxPooling2D(pool_size=(2, 2)),
-#         layers.Flatten(),
-#         layers.Dropout(0.5),
-#         layers.Dense(10, activation="softmax"),
-#     ]
-# )
+model = keras.Sequential(
+    [
+        keras.Input(shape=(28, 28, 1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation="softmax"),
+    ]
+)
 
-# model.compile(optimizer='adam',
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-# history = model.fit(
-#     x_train,
-#     y_train,
-#     epochs=10,
-#     validation_data=(x_test, y_test)
-# )
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=10,
+    validation_data=(x_test, y_test)
+)
 
 img = tf.keras.preprocessing.image.load_img("3.png")
 
 img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.image.rgb_to_grayscale(img_array)
 img_array = np.array([img_array])
-# img_array = cv2.bitwise_not(img_array)
+img_array = cv2.bitwise_not(img_array)
 
 plt.imshow(img_array[0])
 plt.show()
 
-# predictions = model.predict(img_array)
-# score = tf.nn.softmax(predictions[0])
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
 
-# print(np.argmax(score))
-# print(np.max(score))
+print(np.argmax(score))
+print(np.max(score))
 
